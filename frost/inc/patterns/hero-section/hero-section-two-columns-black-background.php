6c786526ac7cba8637a31477176818173e2b47af
Brian Gardner <bmg1227@gmail.com>
2022-01-01 08:51:35 -0600

    Frost: Convert patterns from columns to media and text.

--- frost/inc/patterns/hero-section/hero-section-two-columns-black-background.php
+++ frost/inc/patterns/hero-section/hero-section-two-columns-black-background.php
@@ -1,26 +1,25 @@
 <?php
 /**
- * Frost: Hero with image, text, button, black background
+ * Frost: Hero with media, text, button, black background
  *
  * @package Frost
  */
 
 return array(
-	'title'      => __( 'Hero section with image, text, buttons.', 'frost' ),
+	'title'      => __( 'Section with media, text, buttons.', 'frost' ),
 	'categories' => array( 'frost-hero-section' ),
 	'content'    => '<!-- wp:group {"align":"full","backgroundColor":"black","layout":{"inherit":true}} -->
 				<div class="wp-block-group alignfull has-black-background-color has-background"><!-- wp:spacer -->
 				<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
 				<!-- /wp:spacer -->
 				
-				<!-- wp:columns {"align":"wide"} -->
-				<div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center"} -->
-				<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"textColor":"white","fontSize":"x-large"} -->
-				<h2 class="has-white-color has-text-color has-x-large-font-size" id="text-on-left-image-on-right">Text on left, image on right.</h2>
+				<!-- wp:media-text {"mediaPosition":"right","mediaId":3484,"mediaLink":"https://frostwp.com/patterns/hero-section/sample-white_1200x1200/","mediaType":"image","mediaWidth":40} -->
+				<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile" style="grid-template-columns:auto 40%"><figure class="wp-block-media-text__media"><img src="https://frostwp.com/wp-content/uploads/2021/12/sample-white_1200x1200-1024x1024.jpg" alt="Frost WordPress Theme" class="wp-image-3484 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:heading {"textColor":"white","fontSize":"x-large"} -->
+				<h2 class="has-white-color has-text-color has-x-large-font-size" id="text-on-left-image-on-right">Text on left, media on right.</h2>
 				<!-- /wp:heading -->
 				
 				<!-- wp:paragraph {"textColor":"white"} -->
-				<p class="has-white-color has-text-color">Lorem ipsum dolor sit amet, consectetur adipiscing vestibulum. Fringilla nec accumsan eget, facilisis mi justo, luctus pellentesque vitae gravida non diam accumsan posuere, venenatis at mi turpis.</p>
+				<p class="has-white-color has-text-color">Lorem ipsum dolor sit amet, consectetur adipiscing vestibulum. Fringilla nec accumsan eget, facilisis mi justo, luctus pellentesque gravida vitae non diam accumsan posuere, venenatis mi turpis.</p>
 				<!-- /wp:paragraph -->
 				
 				<!-- wp:buttons -->
@@ -31,18 +30,11 @@ return array(
 				<!-- wp:button {"style":{"border":{"radius":0}},"className":"is-style-outline-white"} -->
 				<div class="wp-block-button is-style-outline-white"><a class="wp-block-button__link no-border-radius">Learn More</a></div>
 				<!-- /wp:button --></div>
-				<!-- /wp:buttons --></div>
-				<!-- /wp:column -->
+				<!-- /wp:buttons --></div></div>
+				<!-- /wp:media-text -->
 				
-				<!-- wp:column {"width":""} -->
-				<div class="wp-block-column"><!-- wp:image {"id":3484,"sizeSlug":"full","linkDestination":"none"} -->
-				<figure class="wp-block-image size-full"><img src="https://frostwp.com/wp-content/uploads/2021/12/sample-white_1200x1200.jpg" alt="Frost WordPress Theme" class="wp-image-3484"/></figure>
-				<!-- /wp:image --></div>
-				<!-- /wp:column --></div>
-				<!-- /wp:columns -->
-				
-				<!-- wp:spacer {"height":40} -->
-				<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- wp:spacer -->
+				<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
 				<!-- /wp:spacer --></div>
 				<!-- /wp:group -->',
 );
--- frost/inc/patterns/hero-section/hero-section-two-columns.php
+++ frost/inc/patterns/hero-section/hero-section-two-columns.php
@@ -1,48 +1,40 @@
 <?php
 /**
- * Frost: Hero section with image, text, button
+ * Frost: Hero section with media, text, button
  *
  * @package Frost
  */
 
 return array(
-	'title'      => __( 'Hero section with image, text, buttons.', 'frost' ),
+	'title'      => __( 'Section with media, text, buttons.', 'frost' ),
 	'categories' => array( 'frost-hero-section' ),
 	'content'    => '<!-- wp:group {"align":"full","backgroundColor":"white","layout":{"inherit":true}} -->
 				<div class="wp-block-group alignfull has-white-background-color has-background"><!-- wp:spacer -->
 				<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
 				<!-- /wp:spacer -->
 				
-				<!-- wp:columns {"align":"wide"} -->
-				<div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center"} -->
-				<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"fontSize":"x-large"} -->
-				<h2 class="has-x-large-font-size" id="text-on-left-image-on-right">Text on left, image on right.</h2>
+				<!-- wp:media-text {"mediaPosition":"right","mediaId":3485,"mediaLink":"https://frostwp.com/patterns/hero-section/sample-black_1200x1200/","mediaType":"image","mediaWidth":40} -->
+				<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile" style="grid-template-columns:auto 40%"><figure class="wp-block-media-text__media"><img src="https://frostwp.com/wp-content/uploads/2021/12/sample-black_1200x1200-1024x1024.jpg" alt="Frost WordPress Theme" class="wp-image-3485 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:heading {"fontSize":"x-large"} -->
+				<h2 class="has-x-large-font-size" id="text-on-left-image-on-right">Text on left, media on right.</h2>
 				<!-- /wp:heading -->
 				
 				<!-- wp:paragraph -->
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing vestibulum. Fringilla nec accumsan eget, facilisis mi justo, luctus pellentesque vitae gravida non diam accumsan posuere, venenatis at mi turpis.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing vestibulum. Fringilla nec accumsan eget, facilisis mi justo, luctus pellentesque gravida vitae non diam accumsan posuere, venenatis mi turpis.</p>
 				<!-- /wp:paragraph -->
 				
 				<!-- wp:buttons -->
-				<div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":0}}} -->
-				<div class="wp-block-button"><a class="wp-block-button__link no-border-radius">Get Started</a></div>
+				<div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":0}},"className":"is-style-fill"} -->
+				<div class="wp-block-button is-style-fill"><a class="wp-block-button__link no-border-radius">Get Started</a></div>
 				<!-- /wp:button -->
 				
 				<!-- wp:button {"style":{"border":{"radius":0}},"className":"is-style-outline"} -->
 				<div class="wp-block-button is-style-outline"><a class="wp-block-button__link no-border-radius">Learn More</a></div>
 				<!-- /wp:button --></div>
-				<!-- /wp:buttons --></div>
-				<!-- /wp:column -->
+				<!-- /wp:buttons --></div></div>
+				<!-- /wp:media-text -->
 				
-				<!-- wp:column {"width":""} -->
-				<div class="wp-block-column"><!-- wp:image {"id":3485,"sizeSlug":"large","linkDestination":"none"} -->
-				<figure class="wp-block-image size-large"><img src="https://frostwp.com/wp-content/uploads/2021/12/sample-black_1200x1200-1024x1024.jpg" alt="Frost WordPress Theme" class="wp-image-3485"/></figure>
-				<!-- /wp:image --></div>
-				<!-- /wp:column --></div>
-				<!-- /wp:columns -->
-				
-				<!-- wp:spacer {"height":40} -->
-				<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- wp:spacer -->
+				<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
 				<!-- /wp:spacer --></div>
 				<!-- /wp:group -->',
 );
