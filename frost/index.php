fd1c611ae5da045a2212e90c82e4acc20fbe46f8
Nick Diego <ndiego@outermost.co>
2022-01-24 08:23:20 -0500

    Frost: Remove alert (closes #19)

--- frost/index.php
+++ frost/index.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * This file adds functions to the Frost theme for WordPress.
- *
- * @package Frost
- * @author  WP Engine
- * @license GNU General Public License v2 or later
- * @link    https://frostwp.com/
- */
-
-if ( current_user_can( 'activate_plugins' ) ) {
-	esc_html_e( 'This theme was built for the WordPress experimental full-site-editing feature. You need to install and activate the Gutenberg plugin to make it work.', 'frost' );
-}
+// There is nothing output here because block themes do not use php templates.
+// There is a core ticket discussing removing this requirement for block themes:
+// https://core.trac.wordpress.org/ticket/54272.
