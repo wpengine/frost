05898d2b8f3fcff3f40e208a5dcbc7988f4c4c48
Nick Diego <ndiego@outermost.co>
2022-03-04 10:01:26 -0500

    Update footer mega patterns.

--- inc/patterns/footer/footer-mega-black-background.php
+++ inc/patterns/footer/footer-mega-black-background.php
@@ -11,8 +11,8 @@ return array(
 	'blockTypes'    => array( 'core/template-part/footer' ),
 	'viewportWidth' => 1280,
 	'content'       => '<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"fontSize":"18px"}},"backgroundColor":"black","textColor":"white","layout":{"inherit":true}} -->
-				<div class="wp-block-group alignfull has-white-color has-black-background-color has-text-color has-background has-link-color" style="font-size:18px"><!-- wp:columns {"align":"wide","style":{"elements":{"link":{"color":[]}},"spacing":{"padding":{"top":"100px","bottom":"70px"}}}} -->
-				<div class="wp-block-columns alignwide has-link-color" style="padding-top:100px;padding-bottom:70px"><!-- wp:column {"width":"50%"} -->
+				<div class="wp-block-group alignfull has-white-color has-black-background-color has-text-color has-background has-link-color" style="font-size:18px"><!-- wp:columns {"align":"wide","style":{"elements":{"link":{"color":[]}},"spacing":{"padding":{"top":"100px","bottom":"100px"},"blockGap":"100px"}}} -->
+				<div class="wp-block-columns alignwide has-link-color" style="padding-top:100px;padding-bottom:100px"><!-- wp:column {"width":"50%"} -->
 				<div class="wp-block-column" style="flex-basis:50%"><!-- wp:heading {"level":4} -->
 				<h4 id="our-company">' . esc_html__( 'Our Company', 'frost' ) . '</h4>
 				<!-- /wp:heading -->
@@ -28,12 +28,10 @@ return array(
 				<!-- /wp:buttons --></div>
 				<!-- /wp:column -->
 
-				<!-- wp:column {"width":"5%"} -->
-				<div class="wp-block-column" style="flex-basis:5%"></div>
-				<!-- /wp:column -->
-
-				<!-- wp:column {"width":"15%"} -->
-				<div class="wp-block-column" style="flex-basis:15%"><!-- wp:heading {"level":4} -->
+				<!-- wp:column -->
+				<div class="wp-block-column"><!-- wp:columns -->
+				<div class="wp-block-columns"><!-- wp:column {"width":"33%"} -->
+				<div class="wp-block-column" style="flex-basis:33%"><!-- wp:heading {"level":4} -->
 				<h4 id="about-us">' . esc_html__( 'About Us', 'frost' ) . '</h4>
 				<!-- /wp:heading -->
 
@@ -47,8 +45,8 @@ return array(
 				<!-- /wp:list --></div>
 				<!-- /wp:column -->
 
-				<!-- wp:column {"width":"15%"} -->
-				<div class="wp-block-column" style="flex-basis:15%"><!-- wp:heading {"level":4} -->
+				<!-- wp:column {"width":"33%"} -->
+				<div class="wp-block-column" style="flex-basis:33%"><!-- wp:heading {"level":4} -->
 				<h4 id="services">' . esc_html__( 'Services', 'frost' ) . '</h4>
 				<!-- /wp:heading -->
 
@@ -62,14 +60,16 @@ return array(
 				<!-- /wp:list --></div>
 				<!-- /wp:column -->
 
-				<!-- wp:column {"width":"15%"} -->
-				<div class="wp-block-column" style="flex-basis:15%"><!-- wp:heading {"level":4} -->
+				<!-- wp:column {"width":"33%"} -->
+				<div class="wp-block-column" style="flex-basis:33%"><!-- wp:heading {"level":4} -->
 				<h4 id="connect">' . esc_html__( 'Connect', 'frost' ) . '</h4>
 				<!-- /wp:heading -->
 
 				<!-- wp:list {"fontSize":"small"} -->
 				<ul class="has-small-font-size"><li><a href="#">Facebook</a></li><li><a href="#">Instagram</a></li><li><a style="font-family: var(--font-family-primary);font-size: var(--font-size-regular);font-weight: var(--font-weight-regular)" href="#">Twitter</a></li><li><a style="font-family: var(--font-family-primary);font-size: var(--font-size-regular);font-weight: var(--font-weight-regular)" href="#">LinkedIn</a></li><li><a href="#">Dribbble</a></li></ul>
 				<!-- /wp:list --></div>
+				<!-- /wp:column --></div>
+				<!-- /wp:columns --></div>
 				<!-- /wp:column --></div>
 				<!-- /wp:columns --></div>
 				<!-- /wp:group -->',
