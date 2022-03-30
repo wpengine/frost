d89a1d64ad54048b99d5f9dfe10ae6a3edf187e1
Brian Gardner <bmg1227@gmail.com>
2022-03-30 08:53:13 -0500

    Move Block Styles to Functions.php (#72)

--- functions.php
+++ functions.php
@@ -83,8 +83,49 @@ function frost_fonts_url() {
 
 }
 
-// Include block styles.
-require get_template_directory() . '/inc/block-styles.php';
+/**
+ * Register block styles.
+ *
+ * @since 0.8.5
+ */
+function frost_register_block_styles() {
+
+	$block_styles = array(
+		'core/button'          => array(
+			'fill-white'    => __( 'Fill White', 'frost' ),
+			'outline-white' => __( 'Outline White', 'frost' ),
+		),
+		'core/group'           => array(
+			'boxshadow'   => __( 'Boxshadow', 'frost' ),
+			'full-height' => __( 'Full-height', 'frost' ),
+		),
+		'core/image'           => array(
+			'boxshadow' => __( 'Boxshadow', 'frost' ),
+		),
+		'core/media-text'      => array(
+			'boxshadow-media' => __( 'Boxshadow', 'frost' ),
+		),
+		'core/navigation-link' => array(
+			'fill'          => __( 'Fill', 'frost' ),
+			'fill-white'    => __( 'Fill White', 'frost' ),
+			'outline'       => __( 'Outline', 'frost' ),
+			'outline-white' => __( 'Outline White', 'frost' ),
+		),
+	);
+
+	foreach ( $block_styles as $block => $styles ) {
+		foreach ( $styles as $style_name => $style_label ) {
+			register_block_style(
+				$block,
+				array(
+					'name'  => $style_name,
+					'label' => $style_label,
+				)
+			);
+		}
+	}
+}
+add_action( 'init', 'frost_register_block_styles' );
 
 // Include block patterns.
 require get_template_directory() . '/inc/block-patterns.php';
