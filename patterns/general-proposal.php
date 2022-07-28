9cc4143602c0fadea469f1f0781fe50dbbd1990d
Aaron Ware <aaron@linchpin.com>
2022-07-28 14:29:13 -0400

    bug: Clean up invalid template

--- patterns/general-proposal.php
+++ patterns/general-proposal.php
@@ -5,27 +5,30 @@
  * Categories: frost-proposals
  * Viewport Width: 1280
  */
-
 ?>
 
 <!-- wp:group {"align":"full"} -->
-<div class="wp-block-group alignfull"><!-- wp:heading {"style":{"spacing":{"margin":{"bottom":"20px"}}}} -->
-	<h2 style="margin-bottom:20px"><?php echo esc_html__( 'Sample Heading', 'ash' ); ?></h2>
+<div class="wp-block-group alignfull"><!-- wp:heading -->
+	<h2>Proposal Page Heading</h2>
 	<!-- /wp:heading -->
 
 	<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top","justifyContent":"left"}} -->
 	<div class="wp-block-group"><!-- wp:group {"align":"full","layout":{"inherit":false,"contentSize":""}} -->
 		<div class="wp-block-group alignfull"><!-- wp:group {"align":"full","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"nowrap"}} -->
 			<div class="wp-block-group alignfull"><!-- wp:paragraph -->
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing vestibulum. Fringilla nec accumsan eget, facilisis mi justo, luctus pellentesque.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				<!-- /wp:paragraph --></div>
 			<!-- /wp:group --></div>
 		<!-- /wp:group -->
 
-		<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"0px"}},"backgroundColor":"lightest-gray","layout":{"inherit":false,"contentSize":"500px","wideSize":"400px"}} -->
-		<div class="wp-block-group has-lightest-gray-background-color has-background" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"className":"is-style-default","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"nowrap"}} -->
-			<div class="wp-block-group is-style-default"><!-- wp:paragraph {"backgroundColor":"lightest-gray","fontSize":"small"} -->
-				<p class="has-lightest-gray-background-color has-background has-small-font-size">orem ipsum dolor sit amet, consectetur adipiscing vestibulum. Fringilla nec accumsan eget, facilisis mi justo, luctus</p>
+		<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"0px"}},"layout":{"inherit":false}} -->
+		<div class="wp-block-group" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","bottom":"20px"}}},"backgroundColor":"lightest-gray","className":"is-style-default","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"nowrap"}} -->
+			<div class="wp-block-group is-style-default has-lightest-gray-background-color has-background" style="padding-top:20px;padding-bottom:20px"><!-- wp:paragraph -->
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Pharetra convallis posuere</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph {"fontSize":"eyebrow"} -->
+				<p class="has-eyebrow-font-size">Lorem ipsum</p>
 				<!-- /wp:paragraph --></div>
 			<!-- /wp:group --></div>
 		<!-- /wp:group --></div>
