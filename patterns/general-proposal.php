f2b6b85ba8e631046ef30e084da83c89968dde40
Aaron Ware <aaron@linchpin.com>
2022-07-29 15:56:51 -0400

    Adding templates

--- patterns/general-proposal.php
+++ patterns/general-proposal.php
@@ -8,21 +8,21 @@
 ?>
 
 <!-- wp:group {"align":"full"} -->
-<div class="wp-block-group alignfull"><!-- wp:heading -->
-	<h2>Proposal Page Heading</h2>
+<div class="wp-block-group alignfull"><!-- wp:heading {"style":{"spacing":{"margin":{"bottom":"20px"}}}} -->
+	<h2 style="margin-bottom:20px">Proposal Page Heading</h2>
 	<!-- /wp:heading -->
 
-	<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top","justifyContent":"left"}} -->
-	<div class="wp-block-group"><!-- wp:group {"align":"full","layout":{"inherit":false,"contentSize":""}} -->
-		<div class="wp-block-group alignfull"><!-- wp:group {"align":"full","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"nowrap"}} -->
+	<!-- wp:group {"className":"medium-flex-wrap","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+	<div class="wp-block-group medium-flex-wrap"><!-- wp:group {"align":"full","className":"flex-auto","layout":{"inherit":false,"contentSize":""}} -->
+		<div class="wp-block-group alignfull flex-auto"><!-- wp:group {"align":"full","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"nowrap"}} -->
 			<div class="wp-block-group alignfull"><!-- wp:paragraph -->
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				<!-- /wp:paragraph --></div>
 			<!-- /wp:group --></div>
 		<!-- /wp:group -->
 
-		<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"0px"}},"layout":{"inherit":false}} -->
-		<div class="wp-block-group" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","bottom":"20px"}}},"backgroundColor":"lightest-gray","className":"is-style-default","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"nowrap"}} -->
+		<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"0px"}},"className":"flex-sidebar","layout":{"inherit":false}} -->
+		<div class="wp-block-group flex-sidebar" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","bottom":"20px"}}},"backgroundColor":"lightest-gray","className":"is-style-default","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"nowrap"}} -->
 			<div class="wp-block-group is-style-default has-lightest-gray-background-color has-background" style="padding-top:20px;padding-bottom:20px"><!-- wp:paragraph -->
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Pharetra convallis posuere</p>
 				<!-- /wp:paragraph -->
