d7300f3a4a70cc5d013251f24bfebf1ac8752fcb
Brian Gardner <bmg1227@gmail.com>
2022-09-19 13:19:06 -0500

    Update Team Member patterns.

--- patterns/general-team-four-columns.php
+++ patterns/general-team-four-columns.php
@@ -6,26 +6,17 @@
  * Viewport Width: 1280
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="margin-top:0px">
-<!-- wp:spacer {"height":100} -->
-<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-<!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
+<!-- wp:group {"layout":{"inherit":true,"type":"constrained"},"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"100px","right":"30px","bottom":"100px","left":"30px"}}}} -->
+<div class="wp-block-group alignfull" style="margin-top:0px;padding-top:100px;padding-right:30px;padding-bottom:100px;padding-left:30px"><!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
 <h2 class="has-text-align-center has-x-large-font-size" id="our-team"><?php echo esc_html__( 'Our Team', 'frost' ); ?></h2>
 <!-- /wp:heading -->
-<!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><?php echo esc_html__( 'The people who are ready to serve you.', 'frost' ); ?></p>
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"bottom":"60px"}}}} -->
+<p class="has-text-align-center" style="margin-bottom:60px"><?php echo esc_html__( 'The people who are ready to serve you.', 'frost' ); ?></p>
 <!-- /wp:paragraph -->
-<!-- wp:spacer {"height":60} -->
-<div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
 <!-- wp:columns {"align":"wide"} -->
-<div class="wp-block-columns alignwide">
-<!-- wp:column -->
-<div class="wp-block-column">
-<!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>"  alt="<?php echo esc_attr__( 'Sample Image', 'frost' ); ?>" class="wp-image-3489"/></figure>
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://frostwp.com/wp-content/themes/frost/assets/images/sample-black_800x800.jpg" alt="Sample Image" class="wp-image-3489"/></figure>
 <!-- /wp:image -->
 <!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
 <h3 class="has-text-align-center has-medium-font-size" id="member-name-1"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
@@ -35,13 +26,11 @@
 <!-- /wp:paragraph -->
 <!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"18px"}}} -->
 <p class="has-text-align-center" style="font-size:18px"><a href="#"><?php echo esc_html__( 'View profile', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph -->
-</div>
+<!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 <!-- wp:column -->
-<div class="wp-block-column">
-<!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>"  alt="<?php echo esc_attr__( 'Sample Image', 'frost' ); ?>" class="wp-image-3489"/></figure>
+<div class="wp-block-column"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://frostwp.com/wp-content/themes/frost/assets/images/sample-black_800x800.jpg" alt="Sample Image" class="wp-image-3489"/></figure>
 <!-- /wp:image -->
 <!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
 <h3 class="has-text-align-center has-medium-font-size" id="member-name-2"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
@@ -51,13 +40,11 @@
 <!-- /wp:paragraph -->
 <!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"18px"}}} -->
 <p class="has-text-align-center" style="font-size:18px"><a href="#"><?php echo esc_html__( 'View profile', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph -->
-</div>
+<!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 <!-- wp:column -->
-<div class="wp-block-column">
-<!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>"  alt="<?php echo esc_attr__( 'Sample Image', 'frost' ); ?>" class="wp-image-3489"/></figure>
+<div class="wp-block-column"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://frostwp.com/wp-content/themes/frost/assets/images/sample-black_800x800.jpg" alt="Sample Image" class="wp-image-3489"/></figure>
 <!-- /wp:image -->
 <!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
 <h3 class="has-text-align-center has-medium-font-size" id="member-name-3"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
@@ -67,13 +54,11 @@
 <!-- /wp:paragraph -->
 <!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"18px"}}} -->
 <p class="has-text-align-center" style="font-size:18px"><a href="#"><?php echo esc_html__( 'View profile', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph -->
-</div>
+<!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 <!-- wp:column -->
-<div class="wp-block-column">
-<!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>"  alt="<?php echo esc_attr__( 'Sample Image', 'frost' ); ?>" class="wp-image-3489"/></figure>
+<div class="wp-block-column"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://frostwp.com/wp-content/themes/frost/assets/images/sample-black_800x800.jpg" alt="Sample Image" class="wp-image-3489"/></figure>
 <!-- /wp:image -->
 <!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
 <h3 class="has-text-align-center has-medium-font-size" id="member-name-4"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
@@ -83,13 +68,7 @@
 <!-- /wp:paragraph -->
 <!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"18px"}}} -->
 <p class="has-text-align-center" style="font-size:18px"><a href="#"><?php echo esc_html__( 'View profile', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph -->
-</div>
-<!-- /wp:column -->
-</div>
-<!-- /wp:columns -->
-<!-- wp:spacer {"height":100} -->
-<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-</div>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
 <!-- /wp:group -->
--- patterns/general-team-two-columns.php
+++ patterns/general-team-two-columns.php
@@ -6,164 +6,109 @@
  * Viewport Width: 1280
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="margin-top:0px">
-<!-- wp:spacer {"height":100} -->
-<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-<!-- wp:group {"align":"wide"} -->
-<div class="wp-block-group alignwide">
-<!-- wp:heading {"fontSize":"x-large"} -->
+<!-- wp:group {"layout":{"inherit":true,"type":"constrained"},"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"100px","right":"30px","bottom":"70px","left":"30px"}}}} -->
+<div class="wp-block-group alignfull" style="margin-top:0px;padding-top:100px;padding-right:30px;padding-bottom:70px;padding-left:30px"><!-- wp:group {"align":"wide"} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"fontSize":"x-large"} -->
 <h2 class="has-x-large-font-size" id="our-team"><?php echo esc_html__( 'Our Team', 'frost' ); ?></h2>
 <!-- /wp:heading -->
-<!-- wp:paragraph -->
-<p><?php echo esc_html__( 'The people who are ready to serve you.', 'frost' ); ?></p>
-<!-- /wp:paragraph -->
-</div>
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"bottom":"60px"}}}} -->
+<p style="margin-bottom:60px"><?php echo esc_html__( 'The people who are ready to serve you.', 'frost' ); ?></p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group -->
-<!-- wp:spacer {"height":60} -->
-<div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
 <!-- wp:columns {"align":"wide"} -->
-<div class="wp-block-columns alignwide">
-<!-- wp:column -->
-<div class="wp-block-column">
-<!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"30px"}}}} -->
-<div class="wp-block-columns" style="margin-bottom:30px">
-<!-- wp:column {"width":"22%"} -->
-<div class="wp-block-column" style="flex-basis:22%">
-<!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>"  alt="<?php echo esc_attr__( 'Sample Image', 'frost' ); ?>" class="wp-image-3489"/></figure>
-<!-- /wp:image -->
-</div>
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"30px"}}}} -->
+<div class="wp-block-columns" style="margin-bottom:30px"><!-- wp:column {"width":"22%"} -->
+<div class="wp-block-column" style="flex-basis:22%"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:column -->
 <!-- wp:column {"width":"75%"} -->
-<div class="wp-block-column" style="flex-basis:75%">
-<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+<div class="wp-block-column" style="flex-basis:75%"><!-- wp:heading {"level":3,"fontSize":"medium"} -->
 <h3 class="has-medium-font-size" id="member-name-1"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
 <!-- /wp:heading -->
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"}}} -->
 <p style="font-size:18px">Lorem ipsum dolor sit amet, consectetur adipiscing lectus. Vestibulum mi justo, luctus eu pellentesque vitae gravida non et aliquam volutpat. <a href="#"><?php echo esc_html__( 'Read more', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph -->
-</div>
-<!-- /wp:column -->
-</div>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
 <!-- /wp:columns -->
 <!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"30px"}}}} -->
-<div class="wp-block-columns" style="margin-bottom:30px">
-<!-- wp:column {"width":"22%"} -->
-<div class="wp-block-column" style="flex-basis:22%">
-<!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>"  alt="<?php echo esc_attr__( 'Sample Image', 'frost' ); ?>" class="wp-image-3489"/></figure>
-<!-- /wp:image -->
-</div>
+<div class="wp-block-columns" style="margin-bottom:30px"><!-- wp:column {"width":"22%"} -->
+<div class="wp-block-column" style="flex-basis:22%"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:column -->
 <!-- wp:column {"width":"75%"} -->
-<div class="wp-block-column" style="flex-basis:75%">
-<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+<div class="wp-block-column" style="flex-basis:75%"><!-- wp:heading {"level":3,"fontSize":"medium"} -->
 <h3 class="has-medium-font-size" id="member-name-2"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
 <!-- /wp:heading -->
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"}}} -->
 <p style="font-size:18px">Lorem ipsum dolor sit amet, consectetur adipiscing lectus. Vestibulum mi justo, luctus eu pellentesque vitae gravida non et aliquam volutpat. <a href="#"><?php echo esc_html__( 'Read more', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph -->
-</div>
-<!-- /wp:column -->
-</div>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
 <!-- /wp:columns -->
 <!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"30px"}}}} -->
-<div class="wp-block-columns" style="margin-bottom:30px">
-<!-- wp:column {"width":"22%"} -->
-<div class="wp-block-column" style="flex-basis:22%">
-<!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>"  alt="<?php echo esc_attr__( 'Sample Image', 'frost' ); ?>" class="wp-image-3489"/></figure>
-<!-- /wp:image -->
-</div>
+<div class="wp-block-columns" style="margin-bottom:30px"><!-- wp:column {"width":"22%"} -->
+<div class="wp-block-column" style="flex-basis:22%"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:column -->
 <!-- wp:column {"width":"75%"} -->
-<div class="wp-block-column" style="flex-basis:75%">
-<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+<div class="wp-block-column" style="flex-basis:75%"><!-- wp:heading {"level":3,"fontSize":"medium"} -->
 <h3 class="has-medium-font-size" id="member-name"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
 <!-- /wp:heading -->
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"}}} -->
 <p style="font-size:18px">Lorem ipsum dolor sit amet, consectetur adipiscing lectus. Vestibulum mi justo, luctus eu pellentesque vitae gravida non et aliquam volutpat. <a href="#"><?php echo esc_html__( 'Read more', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph -->
-</div>
-<!-- /wp:column -->
-</div>
-<!-- /wp:columns -->
-</div>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
 <!-- /wp:column -->
 <!-- wp:column -->
-<div class="wp-block-column">
-<!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"30px"}}}} -->
-<div class="wp-block-columns" style="margin-bottom:30px">
-<!-- wp:column {"width":"22%"} -->
-<div class="wp-block-column" style="flex-basis:22%">
-<!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>"  alt="<?php echo esc_attr__( 'Sample Image', 'frost' ); ?>" class="wp-image-3489"/></figure>
-<!-- /wp:image -->
-</div>
+<div class="wp-block-column"><!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"30px"}}}} -->
+<div class="wp-block-columns" style="margin-bottom:30px"><!-- wp:column {"width":"22%"} -->
+<div class="wp-block-column" style="flex-basis:22%"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:column -->
 <!-- wp:column {"width":"75%"} -->
-<div class="wp-block-column" style="flex-basis:75%">
-<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+<div class="wp-block-column" style="flex-basis:75%"><!-- wp:heading {"level":3,"fontSize":"medium"} -->
 <h3 class="has-medium-font-size" id="member-name"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
 <!-- /wp:heading -->
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"}}} -->
 <p style="font-size:18px">Lorem ipsum dolor sit amet, consectetur adipiscing lectus. Vestibulum mi justo, luctus eu pellentesque vitae gravida non et aliquam volutpat. <a href="#"><?php echo esc_html__( 'Read more', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph -->
-</div>
-<!-- /wp:column -->
-</div>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
 <!-- /wp:columns -->
 <!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"30px"}}}} -->
-<div class="wp-block-columns" style="margin-bottom:30px">
-<!-- wp:column {"width":"22%"} -->
-<div class="wp-block-column" style="flex-basis:22%">
-<!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>"  alt="<?php echo esc_attr__( 'Sample Image', 'frost' ); ?>" class="wp-image-3489"/></figure>
-<!-- /wp:image -->
-</div>
+<div class="wp-block-columns" style="margin-bottom:30px"><!-- wp:column {"width":"22%"} -->
+<div class="wp-block-column" style="flex-basis:22%"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:column -->
 <!-- wp:column {"width":"75%"} -->
-<div class="wp-block-column" style="flex-basis:75%">
-<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+<div class="wp-block-column" style="flex-basis:75%"><!-- wp:heading {"level":3,"fontSize":"medium"} -->
 <h3 class="has-medium-font-size" id="member-name"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
 <!-- /wp:heading -->
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"}}} -->
 <p style="font-size:18px">Lorem ipsum dolor sit amet, consectetur adipiscing lectus. Vestibulum mi justo, luctus eu pellentesque vitae gravida non et aliquam volutpat. <a href="#"><?php echo esc_html__( 'Read more', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph -->
-</div>
-<!-- /wp:column -->
-</div>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
 <!-- /wp:columns -->
 <!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"30px"}}}} -->
-<div class="wp-block-columns" style="margin-bottom:30px">
-<!-- wp:column {"width":"22%"} -->
-<div class="wp-block-column" style="flex-basis:22%">
-<!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>"  alt="<?php echo esc_attr__( 'Sample Image', 'frost' ); ?>" class="wp-image-3489"/></figure>
-<!-- /wp:image -->
-</div>
+<div class="wp-block-columns" style="margin-bottom:30px"><!-- wp:column {"width":"22%"} -->
+<div class="wp-block-column" style="flex-basis:22%"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:column -->
 <!-- wp:column {"width":"75%"} -->
-<div class="wp-block-column" style="flex-basis:75%">
-<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+<div class="wp-block-column" style="flex-basis:75%"><!-- wp:heading {"level":3,"fontSize":"medium"} -->
 <h3 class="has-medium-font-size" id="member-name"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
 <!-- /wp:heading -->
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"}}} -->
 <p style="font-size:18px">Lorem ipsum dolor sit amet, consectetur adipiscing lectus. Vestibulum mi justo, luctus eu pellentesque vitae gravida non et aliquam volutpat. <a href="#"><?php echo esc_html__( 'Read more', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph -->
-</div>
-<!-- /wp:column -->
-</div>
-<!-- /wp:columns -->
-</div>
-<!-- /wp:column -->
-</div>
-<!-- /wp:columns -->
-<!-- wp:spacer {"height":70} -->
-<div style="height:70px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-</div>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
 <!-- /wp:group -->
