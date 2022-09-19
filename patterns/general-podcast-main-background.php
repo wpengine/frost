0e0acd4fcb060fb998fcbb143a2810e5ae4a1fdf
Brian Gardner <bmg1227@gmail.com>
2022-09-19 12:53:57 -0500

    Update Podcast patterns.

--- patterns/general-podcast-main-background.php
+++ patterns/general-podcast-main-background.php
@@ -6,23 +6,15 @@
  * Viewport Width: 1280
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px"}}},"backgroundColor":"foreground","textColor":"background","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-background-color has-foreground-background-color has-text-color has-background" style="margin-top:0px">
-<!-- wp:spacer {"height":100} -->
-<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-<!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"blockGap":"60px"}}} -->
-<div class="wp-block-columns alignwide are-vertically-aligned-center">
-<!-- wp:column {"verticalAlignment":"center","width":"30%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:30%">
-<!-- wp:image {"id":3488,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-white_800x800.jpg'; ?>"  alt="<?php echo esc_attr__( 'Sample Image', 'frost' ); ?>" class="wp-image-3488"/></figure>
-<!-- /wp:image -->
-</div>
+<!-- wp:group {"layout":{"inherit":true,"type":"constrained"},"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"100px","right":"30px","bottom":"100px","left":"30px"}}},"backgroundColor":"foreground","textColor":"background"} -->
+<div class="wp-block-group alignfull has-background-color has-foreground-background-color has-text-color has-background" style="margin-top:0px;padding-top:100px;padding-right:30px;padding-bottom:100px;padding-left:30px"><!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"blockGap":"60px"}}} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"30%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:30%"><!-- wp:image {"id":3488,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-white_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3488"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:column -->
 <!-- wp:column {"verticalAlignment":"center","width":"70%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:70%">
-<!-- wp:heading {"fontSize":"x-large"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:70%"><!-- wp:heading {"fontSize":"x-large"} -->
 <h2 class="has-x-large-font-size" id="sample-podcast-episode"><?php echo esc_html__( 'Sample Podcast Episode', 'frost' ); ?></h2>
 <!-- /wp:heading -->
 <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.5"}}} -->
@@ -32,27 +24,19 @@
 <figure class="wp-block-audio"><audio controls src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/audio/sample-audio-file.mp3'; ?>"></audio></figure>
 <!-- /wp:audio -->
 <!-- wp:buttons {"style":{"spacing":{"blockGap":"10px"}}} -->
-<div class="wp-block-buttons">
-<!-- wp:button {"className":"is-style-outline-background"} -->
-<div class="wp-block-button is-style-outline-background"><a class="wp-block-button__link" href="#">Apple Podcasts</a></div>
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline-background"} -->
+<div class="wp-block-button is-style-outline-background"><a class="wp-block-button__link wp-element-button" href="#">Apple Podcasts</a></div>
 <!-- /wp:button -->
 <!-- wp:button {"className":"is-style-outline-background"} -->
-<div class="wp-block-button is-style-outline-background"><a class="wp-block-button__link" href="#">Google Podcasts</a></div>
+<div class="wp-block-button is-style-outline-background"><a class="wp-block-button__link wp-element-button" href="#">Google Podcasts</a></div>
 <!-- /wp:button -->
 <!-- wp:button {"className":"is-style-outline-background"} -->
-<div class="wp-block-button is-style-outline-background"><a class="wp-block-button__link" href="#">Spotify</a></div>
+<div class="wp-block-button is-style-outline-background"><a class="wp-block-button__link wp-element-button" href="#">Spotify</a></div>
 <!-- /wp:button -->
 <!-- wp:button {"className":"is-style-outline-background"} -->
-<div class="wp-block-button is-style-outline-background"><a class="wp-block-button__link" href="#">Stitcher</a></div>
-<!-- /wp:button -->
-</div>
-<!-- /wp:buttons -->
-</div>
-<!-- /wp:column -->
-</div>
-<!-- /wp:columns -->
-<!-- wp:spacer {"height":100} -->
-<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-</div>
+<div class="wp-block-button is-style-outline-background"><a class="wp-block-button__link wp-element-button" href="#">Stitcher</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
 <!-- /wp:group -->
