b0eb4df5a83765c20327a74a014b4f930d9fb7c6
Brian Gardner <bmg1227@gmail.com>
2022-11-01 09:01:15 -0500

    Add Brand Guide pattern.

---
+++ patterns/general-brand-guide.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Title: Brand guide with columns, text, buttons.
+ * Slug: frost/general-brand-guide
+ * Categories: frost-general
+ * Viewport Width: 1280
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"bottom":"60px","top":"60px","right":"30px","left":"30px"}}},"backgroundColor":"contrast","layout":{"type":"constrained","wideSize":"960px"}} -->
+<div class="wp-block-group alignfull has-contrast-background-color has-background" style="padding-top:60px;padding-right:30px;padding-bottom:60px;padding-left:30px"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"60px","right":"60px","bottom":"60px","left":"60px"}}},"backgroundColor":"base"} -->
+<div class="wp-block-group alignwide has-base-background-color has-background" style="padding-top:60px;padding-right:60px;padding-bottom:60px;padding-left:60px"><!-- wp:heading {"style":{"typography":{"textTransform":"uppercase"},"spacing":{"margin":{"bottom":"20px"}}},"fontSize":"small"} -->
+<h2 class="has-small-font-size" style="margin-bottom:20px;text-transform:uppercase"><?php echo esc_html__( 'Logo Design', 'frost' ); ?></h2>
+<!-- /wp:heading -->
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"30px","left":"30px"}}}} -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":5300,"sizeSlug":"full","linkDestination":"none","style":{"border":{"width":"1px"}},"borderColor":"contrast"} -->
+<figure class="wp-block-image size-full has-custom-border"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-logo-white.jpg'; ?>" alt="Frost Logo" class="has-border-color has-contrast-border-color wp-image-5300" style="border-width:1px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":5301,"sizeSlug":"full","linkDestination":"none","style":{"border":{"width":"1px"}},"borderColor":"contrast"} -->
+<figure class="wp-block-image size-full has-custom-border"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-logo-black.jpg'; ?>" alt="Frost Logo" class="has-border-color has-contrast-border-color wp-image-5301" style="border-width:1px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+<!-- wp:spacer {"height":"80px"} -->
+<div style="height:80px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+<!-- wp:heading {"style":{"typography":{"textTransform":"uppercase"},"spacing":{"margin":{"bottom":"20px"}}},"fontSize":"small"} -->
+<h2 class="has-small-font-size" style="margin-bottom:20px;text-transform:uppercase"><?php echo esc_html__( 'Brand Colors', 'frost' ); ?></h2>
+<!-- /wp:heading -->
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"20px","left":"20px"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-column"><!-- wp:group {"backgroundColor":"contrast"} -->
+<div class="wp-block-group has-contrast-background-color has-background"><!-- wp:spacer {"height":"270px"} -->
+<div style="height:270px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="font-style:normal;font-weight:400">#000000</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+<!-- wp:column {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-column"><!-- wp:group {"backgroundColor":"primary"} -->
+<div class="wp-block-group has-primary-background-color has-background"><!-- wp:spacer {"height":"270px"} -->
+<div style="height:270px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="font-style:normal;font-weight:400">#990099</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+<!-- wp:column {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-column"><!-- wp:group {"backgroundColor":"secondary"} -->
+<div class="wp-block-group has-secondary-background-color has-background"><!-- wp:spacer {"height":"270px"} -->
+<div style="height:270px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="font-style:normal;font-weight:400">#0066FF</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+<!-- wp:column {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-column"><!-- wp:group {"backgroundColor":"tertiary"} -->
+<div class="wp-block-group has-tertiary-background-color has-background"><!-- wp:spacer {"height":"270px"} -->
+<div style="height:270px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="font-style:normal;font-weight:400">#009999</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+<!-- wp:column {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-column"><!-- wp:group {"style":{"border":{"width":"1px"}}} -->
+<div class="wp-block-group" style="border-width:1px"><!-- wp:spacer {"height":"270px"} -->
+<div style="height:270px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="font-style:normal;font-weight:400">#FFFFFF</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+<!-- wp:spacer {"height":"80px"} -->
+<div style="height:80px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"70%"} -->
+<div class="wp-block-column" style="flex-basis:70%"><!-- wp:heading {"style":{"typography":{"textTransform":"uppercase"},"spacing":{"margin":{"bottom":"20px"}}},"fontSize":"small"} -->
+<h2 class="has-small-font-size" style="margin-bottom:20px;text-transform:uppercase">Outfit Regular / Light</h2>
+<!-- /wp:heading -->
+<!-- wp:heading {"fontSize":"x-large"} -->
+<h2 class="has-x-large-font-size"><?php echo esc_html__( 'Sample Heading', 'frost' ); ?></h2>
+<!-- /wp:heading -->
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua. Enimin veniam, quis nostrud exercitation ullamco laboris nisi aliquip taylora commodo consequat. Duis aute irure dolor in voluptate velitar esse cillum dolore eu fugiat nulla pariatur.</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p><a href="#"><?php echo esc_html__( 'This is a sample link', 'frost' ); ?></a>.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+<!-- wp:column {"width":"30%"} -->
+<div class="wp-block-column" style="flex-basis:30%"><!-- wp:heading {"style":{"typography":{"textTransform":"uppercase"},"spacing":{"margin":{"bottom":"20px"}}},"fontSize":"small"} -->
+<h2 class="has-small-font-size" style="margin-bottom:20px;text-transform:uppercase"><?php echo esc_html__( 'Buttons', 'frost' ); ?></h2>
+<!-- /wp:heading -->
+<!-- wp:buttons {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-buttons"><!-- wp:button {"width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link wp-element-button"><?php echo esc_html__( 'Fill', 'frost' ); ?></a></div>
+<!-- /wp:button -->
+<!-- wp:button {"backgroundColor":"primary","textColor":"background","width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-background-color has-primary-background-color has-text-color has-background wp-element-button"><?php echo esc_html__( 'Primary', 'frost' ); ?></a></div>
+<!-- /wp:button -->
+<!-- wp:button {"backgroundColor":"secondary","textColor":"background","width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-background-color has-secondary-background-color has-text-color has-background wp-element-button"><?php echo esc_html__( 'Secondary', 'frost' ); ?></a></div>
+<!-- /wp:button -->
+<!-- wp:button {"backgroundColor":"tertiary","textColor":"background","width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-background-color has-tertiary-background-color has-text-color has-background wp-element-button"><?php echo esc_html__( 'Tertiary', 'frost' ); ?></a></div>
+<!-- /wp:button -->
+<!-- wp:button {"width":100,"className":"is-style-outline"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-outline"><a class="wp-block-button__link wp-element-button"><?php echo esc_html__( 'Outline', 'frost' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
