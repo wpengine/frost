842b8dcb5f25f502696f6c6b7a60a8b80f4b1c74
Brian Gardner <bmg1227@gmail.com>
2023-02-02 12:32:32 -0600

    Update Link Page pattern.

--- patterns/page-link.php
+++ patterns/page-link.php
@@ -2,16 +2,16 @@
 /**
  * Title: Sample link page.
  * Slug: frost/page-link
- * Categories: contact
+ * Categories: featured
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"left":"0px","top":"var:preset|spacing|x-large","right":"0px","bottom":"var:preset|spacing|x-large"}},"dimensions":{"minHeight":"100vh"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
-<div class="wp-block-group alignfull" style="min-height:100vh;margin-top:0px;padding-top:var(--wp--preset--spacing--x-large);padding-right:0px;padding-bottom:var(--wp--preset--spacing--x-large);padding-left:0px"><!-- wp:group {"layout":{"type":"constrained","wideSize":"600px"}} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"left":"0px","top":"var:preset|spacing|large","right":"0px","bottom":"var:preset|spacing|large"}},"dimensions":{"minHeight":"100vh"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull" style="min-height:100vh;margin-top:0px;padding-top:var(--wp--preset--spacing--large);padding-right:0px;padding-bottom:var(--wp--preset--spacing--large);padding-left:0px"><!-- wp:group {"layout":{"type":"constrained","wideSize":"600px"}} -->
 <div class="wp-block-group"><!-- wp:image {"align":"center","id":3489,"width":120,"height":120,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
-<figure class="wp-block-image aligncenter size-full is-resized is-style-rounded"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>" alt="' . esc_attr__( 'Sample Image', 'frost' ); ?&gt;" class="wp-image-3489" width="120" height="120"/></figure>
+<figure class="wp-block-image aligncenter size-full is-resized is-style-rounded"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489" width="120" height="120"/></figure>
 <!-- /wp:image -->
-<!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-x-large-font-size" id="your-name-1"><?php echo esc_html__( 'Your Name', 'frost' ); ?></h2>
+<!-- wp:heading {"textAlign":"center","level":1,"className":"wp-block-heading"} -->
+<h1 class="wp-block-heading has-text-align-center"><?php echo esc_html__( 'Your Name', 'frost' ); ?></h1>
 <!-- /wp:heading -->
 <!-- wp:paragraph {"align":"center"} -->
 <p class="has-text-align-center"><?php echo esc_html__( 'Company Name', 'frost' ); ?><br><a href="mailto:name@company.com">name@company.com</a></p>
@@ -23,30 +23,27 @@
 <!-- wp:social-link {"url":"#","service":"dribbble"} /-->
 <!-- wp:social-link {"url":"#","service":"linkedin"} /--></ul>
 <!-- /wp:social-links -->
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"},"style":{"spacing":{"blockGap":"10px"}}} -->
-<div class="wp-block-buttons">
-<!-- wp:button {"width":100,"style":{"border":{"radius":0}}} -->
-<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link no-border-radius"><?php echo esc_html__( 'Visit My Website', 'frost' ); ?></a></div>
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"},"style":{"spacing":{"blockGap":"10px","margin":{"bottom":"var:preset|spacing|small"}}}} -->
+<div class="wp-block-buttons" style="margin-bottom:var(--wp--preset--spacing--small)">
+<!-- wp:button {"width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link"><?php echo esc_html__( 'Visit My Website', 'frost' ); ?></a></div>
 <!-- /wp:button -->
-<!-- wp:button {"width":100,"style":{"border":{"radius":0}}} -->
-<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link no-border-radius"><?php echo esc_html__( 'Read My Blog', 'frost' ); ?></a></div>
+<!-- wp:button {"width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link"><?php echo esc_html__( 'Read My Blog', 'frost' ); ?></a></div>
 <!-- /wp:button -->
-<!-- wp:button {"width":100,"style":{"border":{"radius":0}}} -->
-<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link no-border-radius"><?php echo esc_html__( 'Download My Ebook', 'frost' ); ?></a></div>
+<!-- wp:button {"width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link"><?php echo esc_html__( 'Download My Ebook', 'frost' ); ?></a></div>
 <!-- /wp:button -->
-<!-- wp:button {"width":100,"style":{"border":{"radius":0}}} -->
-<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link no-border-radius"><?php echo esc_html__( 'Follow My Newsletter', 'frost' ); ?></a></div>
+<!-- wp:button {"width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link"><?php echo esc_html__( 'Follow My Newsletter', 'frost' ); ?></a></div>
 <!-- /wp:button -->
-<!-- wp:button {"width":100,"style":{"border":{"radius":0}}} -->
-<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link no-border-radius"><?php echo esc_html__( 'Listen to My Podcast', 'frost' ); ?></a></div>
+<!-- wp:button {"width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link"><?php echo esc_html__( 'Listen to My Podcast', 'frost' ); ?></a></div>
 <!-- /wp:button -->
 </div>
 <!-- /wp:buttons -->
-<!-- wp:spacer {"height":"40px"} -->
-<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-<!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><a href="https://frostwp.com/"><?php echo esc_html__( 'Made with Frost', 'frost' ); ?></a></p>
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size"><a href="https://frostwp.com/"><?php echo esc_html__( 'Made with Frost', 'frost' ); ?></a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
