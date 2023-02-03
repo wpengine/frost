b376b24d1513a2b7da71ef30ed905b8e5577582c
Brian Gardner <bmg1227@gmail.com>
2023-02-03 12:45:32 -0600

    Update Testimonials patterns.

--- patterns/general-testimonials-contrast-background.php
+++ patterns/general-testimonials-contrast-background.php
@@ -3,42 +3,41 @@
  * Title: Testimonials with text.
  * Slug: frost/general-testimonials-contrast-background
  * Categories: testimonials
- * Viewport Width: 1280
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"var:preset|spacing|x-large","right":"30px","bottom":"var:preset|spacing|x-large","left":"30px"}}},"backgroundColor":"contrast","textColor":"base","className":"has-background-color","layout":{"inherit":true,"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-background-color has-base-color has-contrast-background-color has-text-color has-background" style="margin-top:0px;padding-top:var(--wp--preset--spacing--x-large);padding-right:30px;padding-bottom:var(--wp--preset--spacing--x-large);padding-left:30px"><!-- wp:columns {"align":"wide"} -->
-<div class="wp-block-columns alignwide"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":4,"style":{"typography":{"lineHeight":"1"}},"fontSize":"max-72"} -->
-<h4 class="has-max-72-font-size" style="line-height:1">“</h4>
-<!-- /wp:heading -->
-<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px"}}}} -->
-<p style="margin-top:0px">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae lorem a neque imperdiet sagittis. Vivamus enim velit.</p>
+<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"backgroundColor":"contrast","textColor":"base"} -->
+<div class="wp-block-column has-base-color has-contrast-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1"}},"fontSize":"max-72"} -->
+<p class="has-text-align-center has-max-72-font-size" style="line-height:1">“</p>
 <!-- /wp:paragraph -->
-<!-- wp:paragraph {"fontSize":"small"} -->
-<p class="has-small-font-size"><strong>—Allison Taylor, Designer</strong></p>
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0px"}}}} -->
+<p class="has-text-align-center" style="margin-top:0px"><?php echo esc_html__( 'With its clean, minimal design and powerful features, Frost enables agencies to build cool websites.', 'frost' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size"><strong>—Allison Taylor, Designer</strong></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":4,"style":{"typography":{"lineHeight":"1"}},"fontSize":"max-72"} -->
-<h4 class="has-max-72-font-size" style="line-height:1">“</h4>
-<!-- /wp:heading -->
-<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px"}}}} -->
-<p style="margin-top:0px">Fusce at est sapien. Aliquam tempus et nulla nisipt rhoncus, morbi convallis magna swift. Morbi viverra lobortis ante, volutpat ipsum.</p>
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"backgroundColor":"contrast","textColor":"base"} -->
+<div class="wp-block-column has-base-color has-contrast-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1"}},"fontSize":"max-72"} -->
+<p class="has-text-align-center has-max-72-font-size" style="line-height:1">“</p>
 <!-- /wp:paragraph -->
-<!-- wp:paragraph {"fontSize":"small"} -->
-<p class="has-small-font-size"><strong>—Anthony Breck, Developer</strong></p>
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0px"}}}} -->
+<p class="has-text-align-center" style="margin-top:0px"><?php echo esc_html__( 'With its clean, minimal design and powerful features, Frost enables agencies to build cool websites.', 'frost' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size"><strong>—Anthony Breck, Developer</strong></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":4,"style":{"typography":{"lineHeight":"1"}},"fontSize":"max-72"} -->
-<h4 class="has-max-72-font-size" style="line-height:1">“</h4>
-<!-- /wp:heading -->
-<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px"}}}} -->
-<p style="margin-top:0px">Quisque ullamcorper nulla breu elementum, atipo consectetur ex iaculis quis. Vestibulum et faucibus. Quisque vitae mi pellentesque.</p>
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"backgroundColor":"contrast","textColor":"base"} -->
+<div class="wp-block-column has-base-color has-contrast-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1"}},"fontSize":"max-72"} -->
+<p class="has-text-align-center has-max-72-font-size" style="line-height:1">“</p>
 <!-- /wp:paragraph -->
-<!-- wp:paragraph {"fontSize":"small"} -->
-<p class="has-small-font-size"><strong>—Rebecca Jones, Coach</strong></p>
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0px"}}}} -->
+<p class="has-text-align-center" style="margin-top:0px"><?php echo esc_html__( 'With its clean, minimal design and powerful features, Frost enables agencies to build cool websites.', 'frost' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size"><strong>—Rebecca Jones, Coach</strong></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
---
+++ patterns/general-testimonials-image-contrast-background.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Title: Testimonials with text, image.
+ * Slug: frost/general-testimonials-image-contrast-background
+ * Categories: testimonials
+ */
+?>
+<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"backgroundColor":"contrast","textColor":"base"} -->
+<div class="wp-block-column has-base-color has-contrast-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1"}},"fontSize":"max-72"} -->
+<p class="has-max-72-font-size" style="line-height:1">“</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+<p style="margin-top:0"><?php echo esc_html__( 'With its clean, minimal design and powerful features, Frost enables agencies to build cool websites.', 'frost' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"15px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":299,"width":60,"height":60,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"50px"}},"className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-white_800x800.jpg'; ?>" alt="Testimonial Avatar" class="wp-image-299" style="border-radius:50px" width="60" height="60"/></figure>
+<!-- /wp:image -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"},"typography":{"lineHeight":"1.5"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="line-height:1.5"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><strong>Allison Taylor</strong></p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Designer</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"backgroundColor":"contrast","textColor":"base"} -->
+<div class="wp-block-column has-base-color has-contrast-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1"}},"fontSize":"max-72"} -->
+<p class="has-max-72-font-size" style="line-height:1">“</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+<p style="margin-top:0"><?php echo esc_html__( 'With its clean, minimal design and powerful features, Frost enables agencies to build cool websites.', 'frost' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"15px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":299,"width":60,"height":60,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"50px"}},"className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-white_800x800.jpg'; ?>" alt="Testimonial Avatar" class="wp-image-299" style="border-radius:50px" width="60" height="60"/></figure>
+<!-- /wp:image -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"},"typography":{"lineHeight":"1.5"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="line-height:1.5"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><strong>Anthony Breck</strong></p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Developer</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"backgroundColor":"contrast","textColor":"base"} -->
+<div class="wp-block-column has-base-color has-contrast-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1"}},"fontSize":"max-72"} -->
+<p class="has-max-72-font-size" style="line-height:1">“</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+<p style="margin-top:0"><?php echo esc_html__( 'With its clean, minimal design and powerful features, Frost enables agencies to build cool websites.', 'frost' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"15px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":299,"width":60,"height":60,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"50px"}},"className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/sample-white_800x800.jpg'; ?>" alt="Testimonial Avatar" class="wp-image-299" style="border-radius:50px" width="60" height="60"/></figure>
+<!-- /wp:image -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"},"typography":{"lineHeight":"1.5"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="line-height:1.5"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><strong>Rebecca Jones</strong></p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Coach</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
