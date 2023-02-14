57bfa074ded0df566d2927a774cd74d6e48401ac
Brian Gardner <bmg1227@gmail.com>
2023-02-14 09:29:31 -0600

    Update Team Members patterns.

--- patterns/general-team-four-columns.php
+++ patterns/general-team-four-columns.php
@@ -3,72 +3,73 @@
  * Title: Team with image, text, link.
  * Slug: frost/general-team-four-columns
  * Categories: team
- * Viewport Width: 1280
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"var:preset|spacing|x-large","right":"30px","bottom":"var:preset|spacing|x-large","left":"30px"}}},"layout":{"inherit":true,"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="margin-top:0px;padding-top:var(--wp--preset--spacing--x-large);padding-right:30px;padding-bottom:var(--wp--preset--spacing--x-large);padding-left:30px"><!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
-<h2 class="has-text-align-center has-x-large-font-size" id="our-team"><?php echo esc_html__( 'Our Team', 'frost' ); ?></h2>
-<!-- /wp:heading -->
-<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|medium"}}}} -->
-<p class="has-text-align-center" style="margin-bottom:var(--wp--preset--spacing--medium)">The people who are ready to serve you.</p>
-<!-- /wp:paragraph -->
-<!-- wp:columns {"align":"wide"} -->
-<div class="wp-block-columns alignwide"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/patterns/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489"/></figure>
+<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="margin-top:0"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"align":"center","id":3489,"width":200,"height":200,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/patterns/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489" width="200" height="200"/></figure>
 <!-- /wp:image -->
-<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
-<h3 class="has-text-align-center has-medium-font-size" id="member-name-1"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
+<!-- wp:heading {"textAlign":"center","level":3,"anchor":"member-name-1","fontSize":"medium"} -->
+<h3 class="wp-block-heading has-text-align-center has-medium-font-size" id="member-name-1"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
 <!-- /wp:heading -->
-<!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center">Lorem ipsum dolor sit amet, consectetur et adipiscing.</p>
+<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}}} -->
+<p class="has-text-align-center" style="line-height:1.5"><?php echo esc_html__( 'I’m a WordPress developer and enjoy building websites.', 'frost' ); ?></p>
 <!-- /wp:paragraph -->
-<!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><a href="#"><?php echo esc_html__( 'View profile', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph --></div>
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)"><?php echo esc_html__( 'View Profile', 'frost' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
 <!-- /wp:column -->
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/patterns/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489"/></figure>
+<div class="wp-block-column"><!-- wp:image {"align":"center","id":3489,"width":200,"height":200,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/patterns/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489" width="200" height="200"/></figure>
 <!-- /wp:image -->
-<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
-<h3 class="has-text-align-center has-medium-font-size" id="member-name-2"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
+<!-- wp:heading {"textAlign":"center","level":3,"anchor":"member-name-1","fontSize":"medium"} -->
+<h3 class="wp-block-heading has-text-align-center has-medium-font-size" id="member-name-1"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
 <!-- /wp:heading -->
-<!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center">Lorem ipsum dolor sit amet, consectetur et adipiscing.</p>
+<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}}} -->
+<p class="has-text-align-center" style="line-height:1.5"><?php echo esc_html__( 'I’m a WordPress developer and enjoy building websites.', 'frost' ); ?></p>
 <!-- /wp:paragraph -->
-<!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><a href="#"><?php echo esc_html__( 'View profile', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph --></div>
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)"><?php echo esc_html__( 'View Profile', 'frost' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
 <!-- /wp:column -->
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/patterns/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489"/></figure>
+<div class="wp-block-column"><!-- wp:image {"align":"center","id":3489,"width":200,"height":200,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/patterns/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489" width="200" height="200"/></figure>
 <!-- /wp:image -->
-<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
-<h3 class="has-text-align-center has-medium-font-size" id="member-name-3"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
+<!-- wp:heading {"textAlign":"center","level":3,"anchor":"member-name-1","fontSize":"medium"} -->
+<h3 class="wp-block-heading has-text-align-center has-medium-font-size" id="member-name-1"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
 <!-- /wp:heading -->
-<!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center">Lorem ipsum dolor sit amet, consectetur et adipiscing.</p>
+<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}}} -->
+<p class="has-text-align-center" style="line-height:1.5"><?php echo esc_html__( 'I’m a WordPress developer and enjoy building websites.', 'frost' ); ?></p>
 <!-- /wp:paragraph -->
-<!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><a href="#"><?php echo esc_html__( 'View profile', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph --></div>
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)"><?php echo esc_html__( 'View Profile', 'frost' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
 <!-- /wp:column -->
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:image {"id":3489,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/patterns/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489"/></figure>
+<div class="wp-block-column"><!-- wp:image {"align":"center","id":3489,"width":200,"height":200,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/patterns/images/sample-black_800x800.jpg'; ?>" alt="Sample Image" class="wp-image-3489" width="200" height="200"/></figure>
 <!-- /wp:image -->
-<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
-<h3 class="has-text-align-center has-medium-font-size" id="member-name-4"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
+<!-- wp:heading {"textAlign":"center","level":3,"anchor":"member-name-1","fontSize":"medium"} -->
+<h3 class="wp-block-heading has-text-align-center has-medium-font-size" id="member-name-1"><?php echo esc_html__( 'Member Name', 'frost' ); ?></h3>
 <!-- /wp:heading -->
-<!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center">Lorem ipsum dolor sit amet, consectetur et adipiscing.</p>
+<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}}} -->
+<p class="has-text-align-center" style="line-height:1.5"><?php echo esc_html__( 'I’m a WordPress developer and enjoy building websites.', 'frost' ); ?></p>
 <!-- /wp:paragraph -->
-<!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><a href="#"><?php echo esc_html__( 'View profile', 'frost' ); ?></a>.</p>
-<!-- /wp:paragraph --></div>
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)"><?php echo esc_html__( 'View Profile', 'frost' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
 <!-- /wp:group -->
