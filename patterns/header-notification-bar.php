a64ebe6e9068d0774b23e1a81ba61b9bdc35f5b4
Brian Gardner <bmg1227@gmail.com>
2023-05-11 08:54:52 -0500

    Add Notification Bar patterns. Part of #145

---
+++ patterns/header-notification-bar.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Header notification bar with text, button.
+ * Slug: frost/header-notification-bar
+ * Categories: header
+ * Block Types: core/template-part/header
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"30px","left":"30px","top":"15px","bottom":"15px"},"margin":{"top":"0"}},"border":{"top":{"color":"var:preset|color|contrast","width":"1px"},"bottom":{"color":"var:preset|color|contrast","width":"1px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull" style="border-top-color:var(--wp--preset--color--contrast);border-top-width:1px;border-bottom-color:var(--wp--preset--color--contrast);border-bottom-width:1px;margin-top:0;padding-top:15px;padding-right:30px;padding-bottom:15px;padding-left:30px"><!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}},"fontSize":"x-small"} -->
+<p class="has-text-align-center has-x-small-font-size" style="line-height:1.5"><?php echo esc_html__( 'With its minimal design and powerful feature set, Frost enables agencies to build stylish websites.', 'frost' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"typography":{"fontSize":"14px"},"spacing":{"padding":{"top":"5px","bottom":"5px","left":"15px","right":"15px"}}}} -->
+<div class="wp-block-button has-custom-font-size" style="font-size:14px"><a class="wp-block-button__link wp-element-button" style="padding-top:5px;padding-right:15px;padding-bottom:5px;padding-left:15px"><?php echo esc_html__( 'Get Started', 'frost' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
