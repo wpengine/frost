bc5884a5efe665be75563c636924c35e5175a4b7
Brian Gardner <bmg1227@gmail.com>
2023-05-22 07:40:51 -0500

    Unhide Query pattern, add category type. Part of #144

--- patterns/query.php
+++ patterns/query.php
@@ -2,7 +2,7 @@
 /**
  * Title: List of posts in one column.
  * Slug: frost/query
- * Inserter: false
+ * Categories: posts
  */
 ?>
 <!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"layout":{"type":"constrained"}} -->
