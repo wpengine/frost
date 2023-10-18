a6725a29a9fb4abc075e85f3928ee96dea890955
Brian Gardner <bmg1227@gmail.com>
2023-10-18 09:12:01 -0500

    Semantic HTML Update (posts) (#160)

--- patterns/posts.php
+++ patterns/posts.php
@@ -9,8 +9,10 @@
 <!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"layout":{"type":"constrained"}} -->
 <div class="wp-block-query">
 <!-- wp:post-template -->
-<!-- wp:group {"style":{"spacing":{"blockGap":"10px"}}} -->
-<div class="wp-block-group">
+<!-- wp:group {"tagName":"article","layout":{"type":"default"}} -->
+<article class="wp-block-group">
+<!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"10px"}},"className":"entry-header"} -->
+<header class="wp-block-group entry-header">
 <!-- wp:post-title {"isLink":true} /-->
 <!-- wp:group {"style":{"spacing":{"blockGap":"5px","margin":{"bottom":"30px"}},"typography":{"fontSize":"18px"}},"className":"post-meta","layout":{"type":"flex"}} -->
 <div class="wp-block-group post-meta" style="margin-bottom:30px;font-size:18px">
@@ -21,9 +23,11 @@
 <!-- wp:post-author-name {"isLink":true} /-->
 </div>
 <!-- /wp:group -->
-</div>
+</header>
 <!-- /wp:group -->
 <!-- wp:post-excerpt {"moreText":"Read More"} /-->
+</article>
+<!-- /wp:group -->
 <!-- /wp:post-template -->
 <!-- wp:query-pagination -->
 <!-- wp:query-pagination-previous /-->
