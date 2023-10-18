0e73cace90acc35440e1c02756875feeeb9e0b30
Brian Gardner <bmg1227@gmail.com>
2023-10-18 08:50:41 -0500

    Semantic HTML Update (single posts/comments) (#160)

--- patterns/404.php
+++ patterns/404.php
@@ -2,7 +2,7 @@
 /**
  * Title: 404 error content.
  * Slug: frost/404
- * Inserter: false
+ * Inserter: no
  */
 ?>
 <!-- wp:heading {"level":1} -->
--- patterns/comments.php
+++ patterns/comments.php
@@ -2,39 +2,40 @@
 /**
  * Title: Comments section and form
  * Slug: frost/comments
- * Inserter: false
+ * Inserter: no
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-<!-- wp:comments-query-loop {"className":"site-comments"} -->
-<div class="wp-block-comments-query-loop site-comments">
+<!-- wp:group {"tagName":"section","style":{"spacing":{"margin":{"top":"var:preset|spacing|medium"}}},"className":"entry-comments","layout":{"type":"constrained"}} -->
+<section class="wp-block-group entry-comments" style="margin-top:var(--wp--preset--spacing--medium)">
+<!-- wp:comments {"className":"wp-block-comments-query-loop"} -->
+<div class="wp-block-comments wp-block-comments-query-loop">
+<!-- wp:heading --><h2><?php echo esc_html__( 'Comments', 'frost' ); ?></h2><!-- /wp:heading -->
 <!-- wp:comments-title {"level":3} /-->
-<!-- wp:group {"style":{"spacing":{"margin":{"bottom":"60px"}}}} -->
-<div class="wp-block-group" style="margin-bottom:60px">
+<!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|medium"}}}} -->
+<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--medium)">
 <!-- wp:comment-template -->
-<!-- wp:columns {"style":{"spacing":{"blockGap":"10px","margin":{"bottom":"30px"}}}} -->
-<div class="wp-block-columns" style="margin-bottom:30px">
-<!-- wp:column {"width":"48px"} -->
-<div class="wp-block-column" style="flex-basis:48px">
+<!-- wp:group {"tagName":"article","style":{"spacing":{"blockGap":"var:preset|spacing|x-small","margin":{"bottom":"var:preset|spacing|small"}}},"layout":{"type":"default"}} -->
+<article class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--small)">
+<!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<header class="wp-block-group">
 <!-- wp:avatar {"size":48,"style":{"border":{"radius":"24px"}}} /-->
-</div>
-<!-- /wp:column -->
-<!-- wp:column {"style":{"spacing":{"blockGap":"10px"}}} -->
-<div class="wp-block-column">
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group">
 <!-- wp:comment-author-name /-->
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"},"blockGap":"10px"}},"layout":{"type":"flex"}} -->
-<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px">
+<!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex"}} -->
+<div class="wp-block-group">
 <!-- wp:comment-date /-->
 <!-- wp:comment-edit-link /-->
 </div>
 <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
+</header>
+<!-- /wp:group -->
 <!-- wp:comment-content /-->
 <!-- wp:comment-reply-link /-->
-</div>
-<!-- /wp:column -->
-</div>
-<!-- /wp:columns -->
+</article>
+<!-- /wp:group -->
 <!-- /wp:comment-template -->
 </div>
 <!-- /wp:group -->
@@ -45,6 +46,6 @@
 <!-- /wp:comments-pagination -->
 <!-- wp:post-comments-form /-->
 </div>
-<!-- /wp:comments-query-loop -->
-</div>
+<!-- /wp:comments -->
+</section>
 <!-- /wp:group -->
